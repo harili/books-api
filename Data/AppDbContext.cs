b84482db5839b49e40609d6e94cf7153d7e1db8f
harili <ilieschafyfy@gmail.com>
2021-10-09 00:44:58 +0200

    features : dbcontext init

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using my_books.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> option) : base(option) { }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
---
+++ Data/Models/Book.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace my_books.Models
+{
+    public class Book
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        public int Id { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Title { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Description { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public bool IsRead { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime? DateRead { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public int? Rate { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Genre { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string Author { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public string ConverUrl { get; set; }
+        /// <summary>
+        /// 
+        /// </summary>
+        public DateTime DateAdded { get; set; }
+    }
+}
